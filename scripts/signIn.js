99fa379dac04d010b581cb806168eff7a873401f
SirMo_ <fischer.moritz09@gmail.com>
2024-05-02 17:04:15 +0200

    Chekc email validation

--- scripts/signIn.js
+++ scripts/signIn.js
@@ -14,3 +14,26 @@ function toggleVisibility() {
             invisible = true;
         }
 }
+
+function checkNotNull() {
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let button = document.getElementById('submit-btn');
+    if (email.value == "" && password.value == "" || email.value == "" || password.value == "") {
+        if (email.value == "") {
+            button.type = 'reset';
+            email.style.border = '1px solid red';
+        }
+        else if (password.value == ""){
+            button.type = 'reset';
+            password.style.border = '1px solid red';
+        }
+
+    }
+
+    else {
+        button.type = 'submit';
+        password.style.border = 'none';
+        email.style.border = 'none';
+    }
+};
