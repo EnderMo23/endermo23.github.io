79e73465babb875ede88111f863c96143783ff37
SirSam <samuelheil@web.de>
2024-05-17 15:03:42 +0200

    Signin & signup
    Co-authored-by: SirMo_ <EnderMo23@users.noreply.github.com>

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -20,23 +20,20 @@ export default function SignIn() {
   async function addUser(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const users = db.collection("users");
-    let authData;
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
 
     try {
-      authData = await users.authWithPassword(
-        email,
-        password
-      );
+      await db.collection("users").create({
+        "username": username,
+        "email": email,
+        "emailVisibility": false,
+        "password": password,
+        "passwordConfirm": password
+      });
 
-      //users.requestVerification(email);
-      console.log(db.authStore.isValid);
-      console.log(db.authStore.token);
-      console.log(db.authStore.model?.id);
-
-      router.push('/');
+      router.push('/')
     } catch(error) {
       console.error(error);
     }
@@ -47,29 +44,31 @@ export default function SignIn() {
       <div className={styles.container}>
 
         <div className={styles.header}>
-          Sign in to your Account!
+          Sign up to your Account!
         </div>
 
         <div className={styles.main}>
-          <input type="email" name="email" placeholder="Enter Email"/>
+          
+          <div className={styles.data}>
+            <input type="email" name="email" placeholder="Enter Email"/>
+            <input type="text" name="username" placeholder="Enter Username"/>
+          </div>
+
           <div className={styles.passwordContainer}>   
             <input className={styles.password} name="password" type={invisible ? 'password' : 'text'} placeholder="Enter Password"/>
             <Image src={invisible ? 'invisible.svg' : 'visible.svg'} alt="" className={styles.visible} onClick={toggleVisibility} width={20} height={20}/>
+            <input className={styles.password} name="password" type='password' placeholder="Confirm Password"/>
           </div>
         </div>
 
-        <div className={styles.forgotPassword}>
-          <Link href="/">Forgot Password? - Click here!</Link>
-        </div>
-
         <div className={styles.footer}>
-          <button type="submit" className={styles.submitBtn}>Login</button>
+          <button type="submit" className={styles.submitBtn}>Create Account</button>
         </div>
 
-        <div className={styles.signUp}>
-          <Link href="/signup">
-            Don&apos;t have an account? - <br/>
-            Create one for free here!
+        <div className={styles.signin}>
+          <Link href="/signin">
+            Already have an account? - <br/>
+            Sign in to it here!
           </Link>
         </div>
 
