0bcae074e49f76e8736c02e30bbf1bebb4e918a1
SirMo_ <fischer.moritz09@gmail.com>
2024-05-23 11:36:52 +0200

    IDK

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,34 +4,39 @@ import styles from "./page.module.css"
 
 export default function Profile() {
     return(
-        <>
         <div className={styles.sidebar}>
             <ul className={styles.options}>
-        
-                <li>
-                    <input type="checkbox" name="general"/>
-                    <label htmlFor="general">
-                        General
-                    </label>
-                </li>
-                    
-                <li>                        
-                    <input type="checkbox" name="general"/>
-                    <label htmlFor="general">
-                        Overview
-                    </label>
-                </li>
 
-                <li>                        
-                    <input type="checkbox" name="general"/>
-                    <label htmlFor="general">
-                        Payment
-                    </label>
-                </li>
+                <span className="gerneral">
+                    Gerneral
+                </span>
+
+                <span className="overview">
+                    Overview
+                </span>
+
+                <span className="payment">
+                    Payment
+                </span>
+
+                <span className="payment">
+                    Payment
+                </span>
+
+                <span className="payment">
+                    Payment
+                </span>
+
+                <span className="payment">
+                    Payment
+                </span>
+
+                <span className="payment">
+                    Payment
+                </span>
 
             </ul>
             <div className={styles.hr}></div>
         </div>
-        </>
     );
 }
